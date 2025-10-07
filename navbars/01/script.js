e1af25cafb8a53b7feee7ddf62e385c9b09dd740
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-10-07 23:37:58 +0600

    update search box

--- navbars/01/script.js
+++ navbars/01/script.js
@@ -1,13 +1,15 @@
 const body = document.querySelector("body"),
       nav = document.querySelector("nav"),
       modeToggle = document.querySelector(".dark-light"),
-      searchToggle = document.querySelector(".searchBox");
+      searchToggle = document.querySelector(".searchToggle");
 
+      //js code to toggle dark and light mode
       modeToggle.addEventListener('click', ()=>{
             modeToggle.classList.toggle("active");
             body.classList.toggle("dark");
       });
 
+     //js code to toggle search box
       searchToggle.addEventListener('click', ()=>{
             searchToggle.classList.toggle("active");
       });

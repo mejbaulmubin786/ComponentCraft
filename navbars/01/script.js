cbd562b632d93f89bbc1746d9ef6988f57904d20
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-10-08 13:09:27 +0600

    update

--- navbars/01/script.js
+++ navbars/01/script.js
@@ -5,40 +5,42 @@ const body = document.querySelector("body"),
       sidebarOpen = document.querySelector(".sidebarOpen"),
       sidebarClose = document.querySelector(".sidebarClose");
 
-      let getMode = localStorage.getItem("mode");
-            if(getMode && getMode === 'dark-mode'){
-                  body.classList.add("dark");
-            }
-      
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === 'dark-mode') {
+      body.classList.add("dark");
+}
 
-      //js code to toggle dark and light mode
-      modeToggle.addEventListener('click', ()=>{
-            modeToggle.classList.toggle("active");
-            body.classList.toggle("dark");
 
-            // js code to keep user selected mode even page refresh or fill
-            if(!body.classList.contains("dark")){
-                  localStorage.setItem("mode", "light-mode");
-            } else {
-                  localStorage.setItem("mode", "dark-mode");
-            }
-      });
+//js code to toggle dark and light mode
+modeToggle.addEventListener('click', () => {
+      modeToggle.classList.toggle("active");
+      body.classList.toggle("dark");
 
-     //js code to toggle search box
-      searchToggle.addEventListener('click', ()=>{
-            searchToggle.classList.toggle("active");
-      });
+      // js code to keep user selected mode even page refresh or fill
+      if (!body.classList.contains("dark")) {
+            localStorage.setItem("mode", "light-mode");
+      } else {
+            localStorage.setItem("mode", "dark-mode");
+      }
+});
 
-      // js code toggle sidebar
-      sidebarOpen.addEventListener('click', ()=>{
+//js code to toggle search box
+searchToggle.addEventListener('click', () => {
+      searchToggle.classList.toggle("active");
+});
+
+// js code toggle sidebar
+["click", "touchstart"].forEach(evt =>
+      sidebarOpen.addEventListener(evt, () => {
             nav.classList.toggle("active");
-      });
+      })
+);
 
-      body.addEventListener('click', (e)=>{
-            let clickedElm = e.target;
-            if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
-                  nav.classList.remove("active");
-            }
-      });
+body.addEventListener('click', (e) => {
+      let clickedElm = e.target;
+      if (!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")) {
+            nav.classList.remove("active");
+      }
+});
 
 

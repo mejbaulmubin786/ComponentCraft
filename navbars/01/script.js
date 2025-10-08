42b0e873fc1ad077251530b323b0e7ee02ef0b38
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-10-08 12:13:19 +0600

    almost dome

--- navbars/01/script.js
+++ navbars/01/script.js
@@ -1,16 +1,38 @@
 const body = document.querySelector("body"),
       nav = document.querySelector("nav"),
       modeToggle = document.querySelector(".dark-light"),
-      searchToggle = document.querySelector(".searchToggle");
+      searchToggle = document.querySelector(".searchToggle"),
+      sidebarOpen = document.querySelector(".sidebarOpen"),
+      sidebarClose = document.querySelector(".sidebarClose");
 
       //js code to toggle dark and light mode
       modeToggle.addEventListener('click', ()=>{
             modeToggle.classList.toggle("active");
             body.classList.toggle("dark");
+
+            // js code to keep user selected mode even page refresh or fill
+            if(!body.classList.contains("dark")){
+                  localStorage.setItem("mode", "light-mode");
+            } else {
+                  localStorage.setItem("mode", "dark-mode");
+            }
       });
 
      //js code to toggle search box
       searchToggle.addEventListener('click', ()=>{
             searchToggle.classList.toggle("active");
       });
+
+      // js code toggle sidebar
+      sidebarOpen.addEventListener('click', ()=>{
+            nav.classList.toggle("active");
+      });
+
+      body.addEventListener('click', (e)=>{
+            let clickedElm = e.target;
+            if(!clickedElm.classList.contains("sidebarOpen")&& !clickedElm.classList.contains("menu")){
+                  nav.classList.remove("active");
+            }
+      });
+
 
